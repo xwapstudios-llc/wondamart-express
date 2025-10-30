2703ce8075f42b1442f63541c66193777247be86
nketsiah000 <nketsiah000@gmail.com>
2025-10-30 22:41:44 +0000

    Initial commit

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({path: "./.env"});
 
 interface Config {
     port: number;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,33 @@
 import express from 'express';
+import config from './config';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+// Bundle api
+// Get -> return all data bundles
+// Get :id -> return data bundle by id
+// Get :network -> return data bundles by network
+// Get /search?q= -> search data bundles by query
+// Post -> create new data bundle
+// Put :id -> update data bundle by id
+// Delete :id -> delete data bundle by id
+
+
+// Data purchase api
+// api object: {
+//   userId: string;
+//   bundleId: string;
+//   phoneNumber: string;
+//   units: number;
+// }
+// Post /purchase -> purchase data bundle
+
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(config.port, () => {
+    console.log("Server started successfully.");
+    console.log(`Server is running on http://localhost:${config.port}`);
 });
